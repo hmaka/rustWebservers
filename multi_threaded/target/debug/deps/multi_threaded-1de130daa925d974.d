2a10fe6fc6b2f3d5eebf387dd94e9776b26126ac
Havish <Havish747@gmail.com>
2023-06-09 17:54:25 -0400

    Starting multi-thread project and simply copying single-thread project as starting point

---
+++ multi_threaded/target/debug/deps/multi_threaded-1de130daa925d974.d
@@ -0,0 +1,5 @@
+/Users/havishmaka/Desktop/Code/Rust/rustWebservers/multi_threaded/target/debug/deps/multi_threaded-1de130daa925d974.rmeta: src/main.rs
+
+/Users/havishmaka/Desktop/Code/Rust/rustWebservers/multi_threaded/target/debug/deps/multi_threaded-1de130daa925d974.d: src/main.rs
+
+src/main.rs:
---
+++ multi_threaded/target/debug/deps/multi_threaded-a41e84af5c28f3cc.d
@@ -0,0 +1,5 @@
+/Users/havishmaka/Desktop/Code/Rust/rustWebservers/multi_threaded/target/debug/deps/multi_threaded-a41e84af5c28f3cc.rmeta: src/main.rs
+
+/Users/havishmaka/Desktop/Code/Rust/rustWebservers/multi_threaded/target/debug/deps/multi_threaded-a41e84af5c28f3cc.d: src/main.rs
+
+src/main.rs:
