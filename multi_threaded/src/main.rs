1c2992b53c0e951d6fd996250143fd22dba6dc96
Havish <Havish747@gmail.com>
2023-06-11 16:15:27 -0400

    Basic thread-limiting and concurrency done.

--- multi_threaded/src/main.rs
+++ multi_threaded/src/main.rs
@@ -1,31 +1,48 @@
 use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
 };
 
 fn main() {
+    println!("Listening on port 7878");
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let mut thread_count: usize = 0;
+    let max_threads: usize = 2;
+
+    let mut handles = vec![];
 
     for stream in listner.incoming() {
         let stream = stream.unwrap();
-
-        handle_connection(stream);
+        if thread_count < max_threads {
+            let handle = thread::spawn(move || {
+                handle_connection(stream);
+            });
+            handles.push(handle);
+            thread_count += 1;
+        }
+    }
+    for handle in handles {
+        handle.join().unwrap();
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let mut http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    thread::sleep(std::time::Duration::from_secs(3));
+    //    let buf_reader = BufReader::new(&stream);
+    //    let mut http_request: Vec<_> = buf_reader
+    //        .lines()
+    //        .map(|result| result.unwrap())
+    //        .take_while(|line| !line.is_empty())
+    //        .collect();
 
-    let request_line = http_request.remove(0);
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-    } else {
-    };
-    let contents = std::fs::read_to_string(filename).unwrap();
+    //    let request_line = http_request.remove(0);
+    let status_line = "HTTP/1.1 200 OK";
+    //    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+    //   } else {
+    //  };
+    //    let contents = std::fs::read_to_string(filename).unwrap();
+    let contents = "Hello World";
     let content_length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{contents}");
