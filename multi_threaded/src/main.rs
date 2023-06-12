fc350982044dab4216d07782b6ba79d11fc6ff4c
Havish <Havish747@gmail.com>
2023-06-11 23:44:25 -0400

    Added proper reject response if thread limit is reached

--- multi_threaded/src/main.rs
+++ multi_threaded/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let mut handles = vec![];
 
     for stream in listner.incoming() {
-        let stream = stream.unwrap();
+        let mut stream = stream.unwrap();
         let mut temp_thread_count = thread_count.lock().unwrap();
 
         if *temp_thread_count < max_threads {
@@ -27,6 +27,14 @@ fn main() {
                 *thread_count -= 1;
             });
             handles.push(handle);
+        } else {
+            let status_line = "HTTP/1.1 501 INTERNAL SERVER ERROR";
+            let contents = "Server is currently experiencing too many requests";
+            let content_length = contents.len();
+            let response =
+                format!("{status_line}\r\nContent-Length: {content_length}\r\n\r\n{contents}");
+
+            stream.write_all(response.as_bytes()).unwrap();
         }
     }
     for handle in handles {
@@ -38,7 +46,7 @@ fn handle_connection(mut stream: TcpStream) {
     thread::sleep(std::time::Duration::from_secs(3));
     //    let buf_reader = BufReader::new(&stream);
     //    let mut http_request: Vec<_> = buf_reader
-    //        .lines()
+    //      ``  .lines()
     //        .map(|result| result.unwrap())
     //        .take_while(|line| !line.is_empty())
     //        .collect();
