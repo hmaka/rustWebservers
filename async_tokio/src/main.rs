2e286b4d7894c0e5bdb18df6795f26751b1bc6bd
Havish <Havish747@gmail.com>
2023-06-13 15:15:48 -0400

    Basic mirror-pinging

--- async_tokio/src/main.rs
+++ async_tokio/src/main.rs
@@ -1,4 +1,21 @@
+use std::net::SocketAddr;
+
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener: TcpListener = TcpListener::bind("localhost:7878").await.unwrap();
+
+    let (mut socket, address) = listener.accept().await.unwrap();
+
+    loop {
+        let mut input_buffer = [0u8; 1024];
+
+        let bytes_read: usize = socket.read(&mut input_buffer).await.unwrap();
+
+        socket.write_all(&input_buffer[..bytes_read]).await.unwrap();
+    }
 }
