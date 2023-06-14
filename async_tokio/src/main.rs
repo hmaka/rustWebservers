3b2a95c5e60b4859bc6dcde7382ebfea88c95e70
Havish <Havish747@gmail.com>
2023-06-13 20:46:45 -0400

    Finished async chat server implementation

--- async_tokio/src/main.rs
+++ async_tokio/src/main.rs
@@ -1,21 +1,45 @@
 use std::net::SocketAddr;
+use tokio::io::AsyncBufReadExt;
 
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
+    io::{AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
-async fn main() {
+async fn main() -> ! {
     let listener: TcpListener = TcpListener::bind("localhost:7878").await.unwrap();
-
-    let (mut socket, address) = listener.accept().await.unwrap();
+    let (tx, _rx) = broadcast::channel::<(String, SocketAddr)>(10);
 
     loop {
-        let mut input_buffer = [0u8; 1024];
+        let (mut socket, address) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
-        let bytes_read: usize = socket.read(&mut input_buffer).await.unwrap();
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
 
-        socket.write_all(&input_buffer[..bytes_read]).await.unwrap();
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0{
+                            break;
+                        }
+                        tx.send((line.clone(), address)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() =>{
+                        let (msg, other_address) = result.unwrap();
+
+                        if address != other_address{
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+            }
+        });
     }
 }
